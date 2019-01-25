36ff88a8a797fe250a1da8a522df9635ae9b2908
Dustin Schnelle <schnelle@pdx.edu>
2019-01-24 17:22:54 -0800

    Changed the phrasing of some text and reorganized

--- vrs_text.py
+++ vrs_text.py
@@ -1,11 +1,14 @@
-# vrs_text.py by Chana
+# Title: vrs_text.py
+# Author: Aaron Chan (Chana030102) & Dustin Schnelle (schnelled)
+# Date: 1/24/2019
 #
-# Viking Robotics Society - Discord Chatbot
-# =====================
-# String variables needed for Viking Bot
+# String variables needed for the Viking Robotics Society discord bot
+# =============================================================================
 
-
-ASCII_ART = " __      ___ _    _                      \n\
+#=================================================
+# Program Startup Message
+#=================================================
+ASCII_ART = " __      ___ _    _          \n\
  \ \    / (_) |  (_)                      \n\
   \ \  / / _| | ___ _ __   __ _           \n\
    \ \/ / | | |/ / | '_ \ / _` |          \n\
@@ -26,51 +29,53 @@ ASCII_ART = " __      ___ _    _                      \n\
                                 __/ |     \n\
                                |___/      "
 
-#=======================================
-# Other text
-#=======================================
-bot_version = "2018-10-09"
 
-# First bracket should be a mention for the joining user. Second bracket should be text channel mention.
-welcome_text = "Welcome {} to the Discord Server for Viking Robotics Society at Portland State University\n\
-Our group welcomes people of the community and students of any major to join us in learning robotics.\n\
+#=================================================
+# Help Command Text
+#=================================================
+code_mark = "```"
+commands_header = "========__Viking Bot Commands__========\n"
+help_text = "$help ==> Show this message\n\
+$about ==> Get information about test bot\n\
+$info ==> Get information about the Viking Robotics Society\n"
+commands_admin = "\n========__Admin Commands__========\n\
+$linkupdate \"link name\" <new_link> \n ==> Update Availability Poll link stored \
+by Viking Bot. Link must start with http://\n"
+
+#=================================================
+# Discord Embed Text - About the Society
+#=================================================
+footer = "Viking Robotics Society - Portland State University"
+
+title = "Viking Robotics Society"
+description = "We are the Robotics Club at Portland State, open for the community to join us."
+description2 = "Teach robotics and engineering industry skills through projects."
+email = "robotics@pdx.edu"
+website = "http://robotics.ece.pdx.edu"
+resources = "Viking Robotics [Github](https://github.com/pdx-robotics)\n\
+Website for our [3D Printer](http://roboprint.cecs.pdx.edu)"
+join = "Here's how you can join us formally:\n\
+(1) Let us know more about you by filling out the [New Membership Form](https://goo.gl/forms/AUXMLsyf38IpIoJW2).\n\
+(2) Join us on [Orgsync](https://orgsync.com/85238/chapter).\n\
+(3) Participate in our [Weekly Availability Poll for {}]({}) so we can schedule around everyone's schedule.\n"
+
+meet_desc = "Meetings are held in the Intelligent Robotics Lab located in the \
+Fourth Avenue Building basement level in room 70, unless otherwise specified.\n"
+gen_meet_info = "First Friday of every month starting at 6pm\n"
+
+#=================================================
+# Welcome Text
+#=================================================
+welcome_text = "Welcome {} to the Discord Server for Viking Robotics Society at Portland State University!\n\
+Our group welcomes students of any major and people of the community to join us in learning robotics.\n\
 \n\
-Please change your nickname to your real name so we can identify people on the server.\n\
-If we haven't seen you at our meetings yet, be sure to stop by so we know who you are.\
+Please change your nickname to your real name (FirstName LastName), so we can better identify people on the server.\n\
+If we haven't seen you at our meetings yet, be sure to stop by that way we know who you are.\n\
 See our {} channel for more information, or ask for help in this channel and we'll get back to you."
 
-about = "(Version {}) --- I was made by Chana for the Viking Robotics Society at Portland State University. You can find my source code at https://github.com/pdx-robotics/VikingBot"
-
-#=======================================
-# Discord Embed Text - About the Society
-#=======================================
-title = "Viking Robotics Society"
-footer = "Viking Robotics Society - Portland State University"
-description = "We are the Robotics Club at Portland State, open for the community to join us"
-description2 = "Teach robotics and engineering industry skills through projects"
-email = "*robotics@pdx.edu*"
-website = "http://robotics.ece.pdx.edu"
-vrs_join = "Here's how you can join us formally:\n\
-            (1) Let us know more about you by filling out the [New Membership Form](https://goo.gl/forms/AUXMLsyf38IpIoJW2)\n\
-            (2) Join us on [Orgsync](https://orgsync.com/85238/chapter)\n\
-            (3) Participate in our [Weekly Availability Poll for {}]({}) so we can schedule meetings around everyone's schedule.\n"
-
-vrs_location = "Portland State University - Intelligent Robotics Lab (FAB70-09)"
-
-resources = "Viking Robotics [Github](https://github.com/pdx-robotics)\n\
-            Website for our [3D Printer](http://roboprint.cecs.pdx.edu/)"
-
-meet_desc = "Meetings are in Intelligent Robotics Lab located in Fourth Avenue Building basement level room 70 unless otherwise specified\n-----"
-#=======================================
-# Help Command Text
-#=======================================
-commands_header = "========__Viking Bot Commands__========\n"
-
-code_mark = "```"
-
-help_text = "$help  ==> Shows this message\n\
-$about ==> Get information about Viking Bot\n\
-$info  ==> Get information about Viking Robotics Society\n"
-
-commands_admin = "\n========__Admin Commands__========\n\
-$linkupdate \"link name\" <new_link> \n==> Update Availability Poll link stored by Viking Bot. Link must start with http://\n"
+#=================================================
+# Other Text
+#=================================================
+bot_version = "2019-01-23"
+about = "(Version {}) --- I was made to assist the discord channel for Viking \
+Robotics Society at Portland State University"
